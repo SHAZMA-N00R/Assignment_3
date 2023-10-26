16bc5f11b537c67bb07b9559381d5bdb2a433a21
SHAZMA-N00R <phddsf22m004@pucit.edu.pk>
2023-10-26 11:27:48 +0500

    Created local translator branch to track the remote repo's translator branch

--- webapp/main.py
+++ webapp/main.py
@@ -9,23 +9,29 @@ translator = pipeline('translation', model='Helsinki-NLP/opus-mt-en-de')
 
 app = FastAPI()
 
+
 class Body(BaseModel):
     text: str
+
 
 @app.get('/')
 def root():
     return HTMLResponse("<h1>A self-documenting API to interact with a GPT2 model and translate text</h1>")
+
 
 @app.post('/generate')
 def generate_text(body: Body):
     results = generator(body.text, max_length=35, num_return_sequences=1)
     return results[0]
 
+
 @app.post('/translate')
 def translate_text(body: Body):
     translation = translator(body.text)
     return translation[0]
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
